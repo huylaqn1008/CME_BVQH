1735bdd663ebfc603120e421c42e01607e59c3ee
TanHuy1008 <huylaqn1008@gmail.com>
2025-11-24 10:17:22 +0700

    Design basic dashboard page

--- cme-management/src/App.jsx
+++ cme-management/src/App.jsx
@@ -3,13 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashBoard from './components/DashBoard';
+import Home from './components/Home';
+import Employee from './components/Employee';
+import CME from './components/CME';
+import Profile from './components/Profile';
+import Department from './components/Department';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/adminlogin' element={<Login />} />
-        <Route path='/dashboard' element={<DashBoard />} />
+        <Route path="/adminlogin" element={<Login />} />
+        <Route path="/dashboard" element={<DashBoard />}>
+          <Route index element={<Home />} />
+          <Route path="department" element={<Department />} />
+          <Route path="employee" element={<Employee />} />
+          <Route path="CME" element={<CME />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ cme-management/src/components/CME.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CME = () => {
+  return (
+    <div>CME</div>
+  )
+}
+
+export default CME
